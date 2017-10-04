7258f5598b751cc89f89a5aea1a5fdd24761ba9d
liuhubo <liuhubo@hellobike.com>
2017-10-04 14:18:54 +0800

    feat:新加方法

--- src/com/hello/service/UserService.java
+++ src/com/hello/service/UserService.java
@@ -3,10 +3,19 @@
  */
 package com.hello.service;
 
+import com.hello.dao.UserDao;
+import com.hello.model.User;
+
 /**
  * @author liuhubo
  *
  */
 public class UserService {
-
+	public static User getUserById(Long userId) {
+		return UserDao.getUser(userId);
+	}
+	
+	public static void deleteUser(Long userId) {
+		UserDao.deleteUser(userId);
+	}
 }
