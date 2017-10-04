bd637a08db64937be3f21da487a6083a2047618c
liuhubo <liuhubo@hellobike.com>
2017-10-04 19:40:31 +0800

    测试

--- src/com/hello/model/Man.java
+++ src/com/hello/model/Man.java
@@ -1,5 +1,24 @@
 package com.hello.model;
 
 public class Man {
+private Long id;
+private String name;
+private Long age;
+public Long getId() {
+	return id;
+}
+public void setId(Long id) {
+	this.id = id;
+}
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+@Override
+public String toString() {
+	return "Man [id=" + id + ", name=" + name + ", age=" + age + "]";
+}
 
 }
