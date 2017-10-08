69104f5d2a67a8da6ffb42870d060d76dea1f228
liuhubo <liuhubo@hellobike.com>
2017-10-08 17:32:41 +0800

    double测试案例

---
+++ src/com/hello/test/DoubleTest.java
@@ -0,0 +1,11 @@
+package com.hello.test;
+
+public class DoubleTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double a=12;
+		System.out.println(a/0);
+	}
+
+}
