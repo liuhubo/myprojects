b39e285d43fad0ab86ed18f666863c14fb0864d2
liuhubo <liuhubo@hellobike.com>
2017-10-08 17:20:42 +0800

    浮点测试案例

---
+++ src/com/hello/test/FloatTest.java
@@ -0,0 +1,12 @@
+package com.hello.test;
+
+public class FloatTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			float a =1.9f;
+			int b =20;
+			System.out.println(a/0);
+	}
+
+}
