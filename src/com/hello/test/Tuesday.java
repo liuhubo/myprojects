54cb7f7a7d596d006b98b3d9a330591c21e76049
liuhubo <liuhubo@hellobike.com>
2017-10-09 23:22:08 +0800

    测试周二

---
+++ src/com/hello/test/Tuesday.java
@@ -0,0 +1,10 @@
+package com.hello.test;
+
+public class Tuesday {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.println("222222211111");
+	}
+
+}
